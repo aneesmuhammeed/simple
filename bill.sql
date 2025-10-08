7af5e79d8a911a2cf68fd39d1822a43f9d088bd7
aneesmuhammeed <105257170+aneesmuhammeed@users.noreply.github.com>
2025-10-09 00:42:03 +0530

    Update bill.sql

--- bill.sql
+++ bill.sql
@@ -1,25 +1,27 @@
 CREATE TABLE Bill (
-    cust_no INT PRIMARY KEY,
-    units_consumed INT,
-    amount FLOAT
+    cust_no NUMBER PRIMARY KEY,
+    units_consumed NUMBER,
+    Amount NUMBER
 );
 
---next file
+
+
+--new file a.sql
 CREATE OR REPLACE FUNCTION calc_bill (
-    p_cust_no IN INT,
-    p_units IN INT
-) RETURN FLOAT IS
-    v_amount FLOAT;
+    p_cust_no IN NUMBER,
+    p_units IN NUMBER
+) RETURN NUMBER IS
+    v_amount NUMBER;
 BEGIN
     IF p_units <= 100 THEN
         v_amount := p_units * 1.5;
     ELSIF p_units <= 300 THEN
-        v_amount := (100 * 1.5) + ((p_units - 100) * 2.5);
+        v_amount := 100 * 1.5 + (p_units - 100) * 2.5;
     ELSE
-        v_amount := (100 * 1.5) + (200 * 2.5) + ((p_units - 300) * 4);
+        v_amount := 100 * 1.5 + 200 * 2.5 + (p_units - 300) * 4;
     END IF;
 
-    INSERT INTO Bill (cust_no, units_consumed, amount)
+    INSERT INTO Bill (cust_no, units_consumed, Amount)
     VALUES (p_cust_no, p_units, v_amount);
 
     RETURN v_amount;
@@ -27,10 +29,13 @@ END;
 /
 
 
---next  file
+--testing
+SET SERVEROUTPUT ON;
+
 BEGIN
-    DBMS_OUTPUT.PUT_LINE('Bill Amount: ' || calc_bill(101, 350));
+    DBMS_OUTPUT.PUT_LINE('Bill Amount: ' || calc_bill(1001, 350));
 END;
 /
+
 
 
